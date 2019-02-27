887935726f8ee4fca770f7e333a3a4cbbf003d84
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-02-26 19:26:33 -0600

    forgot to add tests

--- test/TestCalculator.java
+++ test/TestCalculator.java
@@ -1,2 +1,16 @@
-public class Calculator {
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class TestCalculator {
+
+    @Test
+    public void testCreation() {
+        Calculator calculator = new Calculator();
+
+        assertNotNull(calculator);
+        assertTrue(calculator instanceof Calculator);
+    }
+
 }
